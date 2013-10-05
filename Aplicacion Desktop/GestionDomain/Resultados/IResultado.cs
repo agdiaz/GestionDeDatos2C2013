9ca60d4bed7ad79d2b1a2d5c201f75505f364858
agdiaz <diaz.adrian.g@gmail.com>
2013-10-05 14:11:29 -0300

    Resultado de una regla de negocio

---
+++ Aplicacion Desktop/GestionDomain/Resultados/IResultado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestionDomain.Resultados
+{
+    public interface IResultado
+    {
+        bool Correcto { get; }
+        IList<string> Mensajes { get; }
+    }
+}
---
+++ Aplicacion Desktop/GestionDomain/Resultados/Resultado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestionDomain.Resultados
+{
+    public class Resultado : IResultado 
+    {
+        public bool Correcto { get; set; }
+        public IList<string> Mensajes { get; set; }
+
+        public Resultado()
+        {
+            Mensajes = new List<string>();
+        }
+    }
+}
