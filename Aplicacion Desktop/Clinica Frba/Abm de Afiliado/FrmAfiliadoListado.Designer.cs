c01a056e90b9068b82bf75fd60ad4c9958de5969
Flor <florxgomez@gmail.com>
2013-10-06 18:08:02 -0300

    filtros de listado afiliados

--- Aplicacion Desktop/Clinica Frba/Abm de Afiliado/FrmAfiliadoListado.Designer.cs
+++ Aplicacion Desktop/Clinica Frba/Abm de Afiliado/FrmAfiliadoListado.Designer.cs
@@ -28,9 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNroAfiliado = new System.Windows.Forms.TextBox();
+            this.tbPlanMedico = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbDocumento = new System.Windows.Forms.TextBox();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbApellido = new System.Windows.Forms.TextBox();
+            this.cbTipoDoc = new System.Windows.Forms.ComboBox();
             this.gbAcciones.SuspendLayout();
             this.gbFiltros.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // gbFiltros
+            // 
+            this.gbFiltros.Controls.Add(this.tbPlanMedico);
+            this.gbFiltros.Controls.Add(this.label1);
+            this.gbFiltros.Controls.Add(this.label2);
+            this.gbFiltros.Controls.Add(this.cbTipoDoc);
+            this.gbFiltros.Controls.Add(this.label6);
+            this.gbFiltros.Controls.Add(this.tbApellido);
+            this.gbFiltros.Controls.Add(this.tbNroAfiliado);
+            this.gbFiltros.Controls.Add(this.label4);
+            this.gbFiltros.Controls.Add(this.tbNombre);
+            this.gbFiltros.Controls.Add(this.label5);
+            this.gbFiltros.Controls.Add(this.tbDocumento);
+            this.gbFiltros.Controls.Add(this.label3);
+            this.gbFiltros.Controls.SetChildIndex(this.label3, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.tbDocumento, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.label5, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.tbNombre, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.label4, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.tbNroAfiliado, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.tbApellido, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.label6, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.cbTipoDoc, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.label2, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.label1, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.tbPlanMedico, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.btnFiltrar, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.btnLimpiar, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N° Afiliado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Plan Medico";
+            // 
+            // tbNroAfiliado
+            // 
+            this.tbNroAfiliado.Location = new System.Drawing.Point(80, 20);
+            this.tbNroAfiliado.Name = "tbNroAfiliado";
+            this.tbNroAfiliado.Size = new System.Drawing.Size(100, 20);
+            this.tbNroAfiliado.TabIndex = 4;
+            // 
+            // tbPlanMedico
+            // 
+            this.tbPlanMedico.Location = new System.Drawing.Point(80, 44);
+            this.tbPlanMedico.Name = "tbPlanMedico";
+            this.tbPlanMedico.Size = new System.Drawing.Size(100, 20);
+            this.tbPlanMedico.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(257, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Apellido";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(430, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Documento";
+            // 
+            // tbDocumento
+            // 
+            this.tbDocumento.Location = new System.Drawing.Point(498, 18);
+            this.tbDocumento.Name = "tbDocumento";
+            this.tbDocumento.Size = new System.Drawing.Size(100, 20);
+            this.tbDocumento.TabIndex = 10;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(307, 51);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(219, 20);
+            this.tbNombre.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(257, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tipo Documento";
+            // 
+            // tbApellido
+            // 
+            this.tbApellido.Location = new System.Drawing.Point(307, 78);
+            this.tbApellido.Name = "tbApellido";
+            this.tbApellido.Size = new System.Drawing.Size(219, 20);
+            this.tbApellido.TabIndex = 13;
+            // 
+            // cbTipoDoc
+            // 
+            this.cbTipoDoc.FormattingEnabled = true;
+            this.cbTipoDoc.Location = new System.Drawing.Point(349, 17);
+            this.cbTipoDoc.Name = "cbTipoDoc";
+            this.cbTipoDoc.Size = new System.Drawing.Size(75, 21);
+            this.cbTipoDoc.TabIndex = 14;
             // 
             // FrmAfiliadoListado
             // 
@@ -41,10 +179,24 @@
             this.Text = "Clinica FRBA - Afiliados - Listado";
             this.gbAcciones.ResumeLayout(false);
             this.gbFiltros.ResumeLayout(false);
+            this.gbFiltros.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbPlanMedico;
+        private System.Windows.Forms.TextBox tbApellido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbNroAfiliado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbDocumento;
+        private System.Windows.Forms.ComboBox cbTipoDoc;
     }
 }
