2e907a9944127c4f712bc9b2b720b2a7f8fe9c17
agdiaz <diaz.adrian.g@gmail.com>
2013-10-06 18:08:12 -0300

    Filtros para listados de plan, especialidad, usuario, estadisticas y rol

--- Aplicacion Desktop/Clinica Frba/Registro de Usuario/FrmUsuarioListado.Designer.cs
+++ Aplicacion Desktop/Clinica Frba/Registro de Usuario/FrmUsuarioListado.Designer.cs
@@ -28,9 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.gbAcciones.SuspendLayout();
             this.gbFiltros.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // gbFiltros
+            // 
+            this.gbFiltros.Controls.Add(this.label1);
+            this.gbFiltros.Controls.Add(this.label2);
+            this.gbFiltros.Controls.Add(this.comboBox1);
+            this.gbFiltros.Controls.Add(this.textBox1);
+            this.gbFiltros.Controls.SetChildIndex(this.textBox1, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.comboBox1, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.label2, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.label1, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.btnFiltrar, 0);
+            this.gbFiltros.Controls.SetChildIndex(this.btnLimpiar, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rol";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(336, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(87, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(336, 21);
+            this.comboBox1.TabIndex = 5;
             // 
             // FrmUsuarioListado
             // 
@@ -41,10 +91,16 @@
             this.Text = "Clinica FRBA - Usuarios - Listado";
             this.gbAcciones.ResumeLayout(false);
             this.gbFiltros.ResumeLayout(false);
+            this.gbFiltros.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
