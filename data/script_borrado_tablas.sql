72d4a8d603ecea5dae906c3a5d5ef7d026ab3f46
demodawid <demodawid@gmail.com>
2013-10-27 12:14:41 -0300

    Script borrado tablas.

---
+++ data/script_borrado_tablas.sql
@@ -0,0 +1,7 @@
+
+DROP TABLE TOP_4.Profesional_Especialidad
+DROP TABLE TOP_4.Especialidad
+DROP TABLE TOP_4.Tipo_especialidad
+DROP TABLE TOP_4.Profesional
+DROP TABLE TOP_4.Usuario
+DROP TABLE TOP_4.Plan_medico
