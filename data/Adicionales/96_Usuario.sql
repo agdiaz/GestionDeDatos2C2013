bd4c2a6984a84c22fd7b63dac9fbd062f1ef77e4
Flor <florxgomez@gmail.com>
2013-10-27 20:47:34 -0300

    sp usuario

--- data/Adicionales/96_Usuario.sql
+++ data/Adicionales/96_Usuario.sql
@@ -9,8 +9,22 @@ SELECT [id_usuario]
       ,[habilitado]
   FROM [TOP_4].[Usuario]
   WHERE username = @p_nombre
-  AND habilitado = 1
+  AND habilitado = '1'
  END
+GO
+
+CREATE PROCEDURE [TOP_4].[sp_Usuario_select_all]
+
+AS
+BEGIN
+SELECT [id_usuario]
+      ,[username]
+      ,[password]
+      ,[cant_intentos_fallidos]
+      ,[habilitado]
+  FROM [GD2C2013].[TOP_4].[Usuario]
+  WHERE habilitado = '1'
+END
 GO
 
 
