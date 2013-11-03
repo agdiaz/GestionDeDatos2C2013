3873444233ba868f793742d720dd3e121c1fd175
agdiaz <diaz.adrian.g@gmail.com>
2013-11-03 12:58:47 -0300

    sobreescribo el tostring para que se muestren bien en las grillas

--- Aplicacion Desktop/GestionCommon/Enums/TipoDocumento.cs
+++ Aplicacion Desktop/GestionCommon/Enums/TipoDocumento.cs
@@ -17,6 +17,11 @@ namespace GestionCommon.Enums
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
     }
 
     public class TipoDocumentoDNI : TipoDocumento
